d0c6744bf6b2a3a7d1e6c387fa3ce0b50a4197d4
faysil-amin <foodbrochandpur@gmail.com>
2026-04-13 10:30:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.SECRET_KEY}@cluster0.rgj7zze.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    const assignment11Server = client.db("assignment11Server");
+    const userCollection = assignment11Server.collection("userCollection");
+    app.post("/user", async (req, res) => {
+      const data = req.body;
+      data.user = "user";
+      const result = await userCollection.insertOne(data);
+      res.send(result);
+    });
+    app.get("/user", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
