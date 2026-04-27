65ba0fba83371b69420e5185a7de8fd75a5fe651
faysil-amin <foodbrochandpur@gmail.com>
2026-04-27 23:20:44 +0200

    addlesson patch set

--- index.js
+++ index.js
@@ -14,17 +14,40 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./assignment-11-firebase-adminsdk.json");
+
+const verifyToken = async (req, res, next) => {
+  const tokenHeader = req.headers.authorization;
+  if (!tokenHeader) {
+    return res.status(401).send({ message: "unauthoriz access" });
+  }
+  const tokenOnly = tokenHeader.split(" ")[1];
+  if (!tokenOnly) {
+    return res.status(401).send({ message: "token not found" });
+  }
+  try {
+    const check = await admin.auth().verifyIdToken(tokenOnly);
+    req.check_email = check.email;
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "unauthoriz access" });
+  }
+};
 async function run() {
   try {
     await client.connect();
     const assignment11Server = client.db("assignment11Server");
     const userCollection = assignment11Server.collection("userCollection");
+    const lessonCollection = assignment11Server.collection("lessonCollection");
     app.post("/user", async (req, res) => {
       const data = req.body;
       const query = { email: req.email };
       const email = req.email;
       data.user = "user";
       data.createdAt = new Date();
+      data.user = "not_premeum";
       const existUser = await userCollection.findOne({ email });
       if (existUser) {
         return res.send({ message: "user exist" });
@@ -42,6 +65,26 @@ async function run() {
       const query = { userEmail: email };
       const result = await userCollection.findOne(query);
       res.send({ role: result?.role || "user" });
+    });
+    app.post("/addlesson", async (req, res) => {
+      const data = req.body;
+      data.createdAt = new Date();
+      const result = await lessonCollection.insertOne(data);
+      res.send(result);
+    });
+    app.patch("/addlesson/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $inc: { lesson_like: 1 },
+      };
+      const result = await lessonCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    app.get("/addlesson", async (req, res) => {
+      const data = lessonCollection.find().sort({ createdAt: -1 });
+      const result = await data.toArray();
+      res.send(result);
     });
     await client.db("admin").command({ ping: 1 });
     console.log(
